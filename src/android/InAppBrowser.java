1b64bcbd5d501a2dca04c4cbbbba344e8ef6bcd8
Mohit Mittal <mohitm@wayforward.io>
2020-06-16 13:12:24 +0530

    test

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1029,9 +1029,8 @@ public class InAppBrowser extends CordovaPlugin {
                 }
 
                 // Enable Thirdparty Cookies on >=Android 5.0 device
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                    CookieManager.getInstance().setAcceptThirdPartyCookies(inAppWebView,true);
-                }
+                CookieManager.getInstance().setAcceptThirdPartyCookies(inAppWebView,true);
+
 
                 inAppWebView.loadUrl(url);
                 inAppWebView.setId(Integer.valueOf(6));
